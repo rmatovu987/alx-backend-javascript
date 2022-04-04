0ab9e108eaa3ad253205620ca8c8536b56912a16
rmatovu987 <rmatovu987@gmail.com>
2022-04-04 20:43:18 +0300

    Task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
-  let employee = [];
-  for (let [depts, emplys] of Object.entries(report.allEmployees)){
-    for (let emp of emplys){
+  const employee = [];
+  for (const [dept, emplys] of Object.entries(report.allEmployees)) {
+    for (const emp of emplys) {
       employee.push(emp);
     }
   }
