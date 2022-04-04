7688aa7338dafdf9f44ec4653680762786c3615a
rmatovu987 <rmatovu987@gmail.com>
2022-04-04 19:48:14 +0300

    Task 11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const data = {
-    [departmentName]: [...employees]
-  };
-  return data;
+  return {[departmentName]: [...employees]};
 }
