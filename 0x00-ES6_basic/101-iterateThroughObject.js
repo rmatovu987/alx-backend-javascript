0f2a97fe69279e5a6ebe509a554b2e7789181beb
rmatovu987 <rmatovu987@gmail.com>
2022-04-05 18:21:39 +0300

    Task 13

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let itemstr = '';
-  let i = 0;
-  for (const item of reportWithIterator) {
-    if (reportWithIterator.length - 1 === i) {
-      itemstr += item;
-    } else {
-      itemstr += item;
-      itemstr += ' | ';
-    }
-    i += 1;
-  }
-  return itemstr;
+  return reportWithIterator.join(' | ');
 }
