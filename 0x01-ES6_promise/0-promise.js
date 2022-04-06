d5a9704400d3bc9b6000370791324244e84fcb3a
rmatovu987 <rmatovu987@gmail.com>
2022-04-06 05:34:50 +0300

    Task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,5 @@
-export function getResponseFromAPI(){
-  return new Promise((resolve, reject) =>{});
+export function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve();
+  });
 }
