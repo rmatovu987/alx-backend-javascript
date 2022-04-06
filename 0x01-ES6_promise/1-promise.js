748dc5b264509caca5fe1755b4fa5eb46c577703
rmatovu987 <rmatovu987@gmail.com>
2022-04-06 06:07:58 +0300

    Task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -4,9 +4,9 @@ export default function getFullResponseFromAPI(success) {
         resolve({
           status: 200,
           body: 'Success'
-        })
+        });
       } else {
-        reject(new Error('The fake API is not working currently'))
+        reject(new Error('The fake API is not working currently'));
       }
     }
   );
