410bb665f6af7ddeed93894600bb18cfb930c9d3
rmatovu987 <rmatovu987@gmail.com>
2022-04-08 03:44:08 +0300

    Task 0

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,35 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = name;
+    this._students = students;
+    this._length = length;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') throw new TypeError('Length must be a number');
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (typeof value !== 'object' || value.every((item) => (typeof item !== 'string'))) throw new TypeError('Students must be an array of strings');
+    this._students = value;
+  }
+}
