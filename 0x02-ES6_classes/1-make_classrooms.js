10ee096309e1b221391b95cb9df261b3a5d4fe6d
rmatovu987 <rmatovu987@gmail.com>
2022-04-08 03:35:38 +0300

    Task 0

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,8 @@
 import Classroom from './0-classroom';
 
 export default function initializeRooms() {
-  return [Classroom(19), Classroom(20), Classroom(34)];
+  return [
+    new Classroom(19),
+    new Classroom(20),
+    new Classroom(34)];
 }
