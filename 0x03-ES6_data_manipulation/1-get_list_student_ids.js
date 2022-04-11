a0640289bd015ed92f3090a4e51cb3732e273a1d
rmatovu987 <rmatovu987@gmail.com>
2022-04-11 19:55:12 +0300

    Task 3

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,11 @@
 export default function getListStudentIds(students) {
   const array = [];
-  if ((students instanceof Array)) {
-    for (const student of students) {
-      array.push(student.id);
-    }
+  if (!(students instanceof Array)) {
+    return [];
+  }
+
+  for (const student of students) {
+    array.push(student.id);
   }
   return array;
 }
