1dd5fa732df5443e03b84a844a8aa5279ffe9602
rmatovu987 <rmatovu987@gmail.com>
2022-04-12 07:37:47 +0300

    Task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -7,10 +7,12 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
     return [];
   }
 
-  return students.filter((std) => std.location === city)
+  const mp = students.filter((std) => std.location === city)
     .map((std) => {
       const [grade] = newGrades.filter((g) => g.studentId === std.id);
       const grd = grade ? grade.grade : 'N/A';
       return { ...std, grd };
     });
+
+  return mp;
 }
