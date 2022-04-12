5d2ef983ad09cc4c0fa6db2eae0dd2703c9d7971
rmatovu987 <rmatovu987@gmail.com>
2022-04-12 09:53:34 +0300

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,14 @@
 export default function cleanSet(set, startString) {
-  if (startString === '' || typeof startString !== 'string') {
-    return '';
-  }
-  if (typeof set !== 'object') {
-    return '';
-  }
-  const newStr = [];
-  for (const item of set) {
-    if (item.startsWith(startString)) {
-      newStr.push(item.slice(startString.length));
+  let text = '';
+  const arr = [];
+
+  if (startString && typeof startString === 'string') {
+    for (const x of set) {
+      if (x && x.startsWith(startString)) {
+        arr.push(x.slice(startString.length));
+      }
     }
+    text = arr.join('-');
   }
-  return newStr.join('-');
+  return text;
 }
