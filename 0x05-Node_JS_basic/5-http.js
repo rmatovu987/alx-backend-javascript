e797425883338933b1151a3711175ee0d1a816bf
rmatovu987 <rmatovu987@gmail.com>
2022-05-31 09:33:52 +0300

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -7,71 +7,50 @@ const port = 1245;
 const path = process.argv[2];
 
 async function countStudents() {
-    const promise = (res, rej) => {
-        fs.readFile(path, 'utf8', (err, dataCollected) => {
-            if (err) {
-                throw new Error('Cannot load the database');
-            }
-            const messages = [];
-            let message;
-            const content = dataCollected.toString().split('\n');
-            let students = content.filter((item) =>
-                item !== 'firstname,lastname,age,field'
-            );
-            students = students.map((item) => item.split(','));
-            const nStudents = students.length ? students.length - 1 : 0;
-            message = `Number of students: ${nStudents}`;
-            // console.log(message);
-            messages.push(message);
-            const subjects = {};
-            for (const i in students) {
-                if (i !== 0) {
-                    if (!subjects[students[i][3]]) subjects[students[i][3]] = [];
-                    subjects[students[i][3]].push(students[i][0]);
-                }
-            }
-            delete subjects.subject;
-            for (const key of Object.keys(subjects)) {
-                if(key !== 'undefined') {
-                    message = `Number of students in ${key}: ${
-                        subjects[key].length
-                    }. List: ${subjects[key].join(', ')}`;
-                    // console.log(message);
-                    messages.push(message);
-                }
-            }
-            res(messages);
-        });
-
-    };
-
-    return new Promise(promise);
+  try {
+    const csvFile = await fs.promises.readFile(path, {encoding: 'utf8'});
+    const csvData = csvFile.split('\n');
+    const csStudent = [];
+    const sweStudent = [];
+    for (let i = 1; i < csvData.length - 1; i += 1) {
+      const line = csvData[i].split(',');
+      if (line[3] === 'CS') {
+        csStudent.push(line[0].trim());
+      } else if (line[3] === 'SWE') {
+        sweStudent.push(line[0].trim());
+      }
+    }
+    return ({
+      csStudent,
+      sweStudent,
+    });
+  } catch (ex) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    if (req.url === '/') {res.end('Hello Holberton School!');}
-    else if (req.url === '/students') {
-        countStudents(path)
-            .then((data) => {
-                res.write('This is the list of our students\n')
-                data = data.map((item, idx) => {
-                    if (idx === data.length-1) {
-                        res.write(item);
-                    } else {
-                        res.write(item + '\n')
-                    }
-                })
-                res.end();
-            })
-            .catch(() => {
-                console.log();
-                res.write('This is the list of our students\n');
-                res.write('Cannot load the database');
-                res.end();
-            });
-    }
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(path)
+      .then(({
+               csStudent,
+               sweStudent,
+             }) => {
+        const sum = csStudent.length + sweStudent.length;
+        res.write('This is the list of our students\n');
+        res.write(`Number of students: ${sum}\n`);
+        res.write(`Number of students in CS: ${csStudent.length}. List: ${csStudent.toString().split(',').join(', ')}\n`);
+        res.write(`Number of students in SWE: ${sweStudent.length}. List: ${sweStudent.toString().split(',').join(', ')}`);
+        res.end();
+      })
+      .catch(() => {
+        res.end('Cannot load the database');
+      });
+  }
 });
 
 app.listen(port);
